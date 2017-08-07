f23a9ded6ce6293d74855bc7a0bdb1bb7685b226
James Voong <voongjame@ecs.vuw.ac.nz>
2017-08-07 19:01:37 +1200

    added functions to handle some commands

--- user_upload.php
+++ user_upload.php
@@ -2,14 +2,51 @@
 
 echo "Script init\n";
 
-$command = readline("Enter name of CSV file to be opened: ");
-$file = fopen("$command", "r");
-while (!feof($file)) {
-    print_r(fgetcsv($file));
-}
-fclose($file);
+commandHandler();
+
+
+//$print = file_exists($command);
+//echo $print;
+
+$handle = fopen($_FILES["users.csv"]["tmp_name"], 'r');
+echo "$handle";
+
+//$file = fopen("$command", "r");
+
+//while (!feof($file)) {
+//    if (feof == true) {
+//        echo "Invalid file name.\n";
+//        break;
+//    }
+//    print_r(fgetcsv($file));
+//}
+//fclose($file);
 
 echo "Script end\n";
+
+function commandHandler()           //Handles inputs from the command line
+{
+    $command = readline("Enter command (enter --help for full commands list): ");       //Asks for the filename to be opened and saves it
+    if (strcmp($command, "--help") == 0) {
+        printHelp();
+    } else {
+        echo "Invalid command. Please enter a valid command.\n";
+        commandHandler();
+    }
+}
+
+function printHelp()                //This function prints out the help menu
+{
+    echo "--file [csv file name] – this is the name of the CSV to be parsed\n"
+    echo "--create_table – this will cause the MySQL users table to be built (and no further action will be taken)\n";
+    echo "--dry_run – this will be used with the --file directive in the instance that we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered.\n";
+    echo "-u – MySQL username\n";
+    echo "-p – MySQL password\n";
+    echo "-h – MySQL host\n";
+
+    commandHandler();
+
+}
 
 /**
  * Created by PhpStorm.
