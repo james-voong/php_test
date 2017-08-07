e8cf5bb235df4ebabe93dc133da96fafa61722f4
James Voong <voongjame@ecs.vuw.ac.nz>
2017-08-07 17:11:30 +1200

    logic_test complete

--- logic_test.php
+++ logic_test.php
@@ -2,9 +2,14 @@
 echo "script init\n";
 
 for ($i = 1; $i <= 100; $i++) {
-    if (($i%3)==0){
-        echo "foo\n";
-    } else echo "$i\n";
+
+    if ($i % 3 == 0 && $i % 5 == 0) {                   //checks if number is divisible by 3 and 5
+        echo "foobar\n";                                //print foobar if it is
+    } else if ($i % 3 == 0) {                           //otherwise check if number is divisible by 3
+        echo "foo\n";                                   //print foo if it is
+    } else if ($i % 5 == 0) {                           //otherwise check if number is divisible by 5
+        echo "bar\n";                                   //print bar if it is
+    } else echo "$i\n";                                 //otherwise echo the number
 
 }
 
