4085f619c2123f8b4b7290bbfbf8d442cd6b72dc
James Voong <voongjame@ecs.vuw.ac.nz>
2017-08-07 16:56:47 +1200

    logic_test now prints foo if divisible by 3

---
+++ logic_test.php
@@ -0,0 +1,17 @@
+<?php
+echo "script init\n";
+
+for ($i = 1; $i <= 100; $i++) {
+    if (($i%3)==0){
+        echo "foo\n";
+    } else echo "$i\n";
+
+}
+
+echo "script end\n";
+/**
+ * Created by PhpStorm.
+ * User: James
+ * Date: 7/08/17
+ * Time: 4:38 PM
+ */
